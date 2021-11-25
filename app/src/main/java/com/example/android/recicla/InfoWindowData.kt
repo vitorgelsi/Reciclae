4a99f685fe64d3fbff3ac987c0d992d0e093e3b4
vitor.v <vitorfv@gmail.com>
2021-11-24 21:43:18 -0300

    Remoção de arquivos não utilizados

--- app/src/main/java/com/example/android/recicla/InfoWindowData.kt
+++
@@ -1,5 +0,0 @@
-package com.example.android.recicla
-
-data class InfoWindowData(val mLocatioName: String,
-                          val mLocationAddres: String,
-                          val mLocationItens: String)
